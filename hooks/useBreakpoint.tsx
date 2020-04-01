ed9b4ae3344ae123551056d8760e614d68c3c0bf
Nick Edgar <edgar@76design.com>
2020-04-01 14:26:29 -0400

    useBreakpoint hook to get the current breakpoint.

---
+++ hooks/useBreakpoint.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { throttle } from "../utils/helpers";
+
+const getDeviceBreakpoint = (width: number) => {
+	if (width < 576) {
+		return "xs";
+	} else if (width >= 576 && width < 768) {
+		return "sm";
+	} else if (width >= 768 && width < 992) {
+		return "md";
+	} else if (width >= 992 && width < 1280) {
+		return "lg";
+	} else if (width >= 1280) {
+		return "xl";
+	}
+};
+
+export const useBreakpoint = () => {
+	const client = typeof window !== "undefined";
+	const [breakpoint, setBreakpoint] = useState(() =>
+		client ? getDeviceBreakpoint(window.innerWidth) : "md",
+	);
+
+	const calculateInnerWidth = throttle(200, () => {
+		setBreakpoint(getDeviceBreakpoint(window.innerWidth));
+	});
+
+	useEffect(() => {
+		if (client) {
+			window.addEventListener("resize", calculateInnerWidth);
+		}
+		return () =>
+			client && window.removeEventListener("resize", calculateInnerWidth);
+	}, []);
+
+	return breakpoint;
+};
