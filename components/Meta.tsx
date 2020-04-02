d87a48c89e7c401b75542f761cc5e2f7a2de9b30
Nick Edgar <edgar@76design.com>
2020-04-02 08:51:10 -0400

    Fix link to favicon.

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -8,7 +8,7 @@ const Meta = () => {
 		<Head>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta charSet="utf-8" />
-			<link rel="shortcut icon" href="/public/static/favicon.png" />
+			<link rel="shortcut icon" href="/static/favicon.png" />
 			<title>{t("common:site-title")}</title>
 		</Head>
 	);
