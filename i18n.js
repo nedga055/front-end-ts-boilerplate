289561cefd5ad0bbc6b1baf3b75577ae8bb717df
Cruickshank <graham.cruickshank@hrsdc-rhdcc.gc.ca>
2020-11-10 12:16:04 -0500

    added backend plugin to preload namespaces

--- i18n.js
+++ i18n.js
@@ -1,33 +1,31 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import common_en from "./public/static/locales/en/common.json";
-import common_fr from "./public/static/locales/fr/common.json";
-import index_en from "./public/static/locales/en/index.json";
-import index_fr from "./public/static/locales/fr/index.json";
-import user_en from "./public/static/locales/en/user.json";
-import user_fr from "./public/static/locales/fr/user.json";
+import Backend from "i18next-http-backend";
+// NOTE: First namespace is made default
+const namespaces = [
+  "common",
+  "index",
+  "user"
+]
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
   .init({
-    resources: {
-      'en': {
-        common: common_en,
-        index: index_en,
-        user: user_en,
-      },
-      'fr': {
-        common: common_fr,
-        index: index_fr,
-        user: user_fr,
-      }
+    react: {
+      useSuspense: false
     },
-    defaultNS: "common",
+    backend: {
+      loadPath: './static/locales/{{lng}}/{{ns}}.json',
+    },
+    ns: namespaces,
+    defaultNS: namespaces[0],
 
     interpolation: {
       escapeValue: false // react already safes from xss
-    }
+    },
+    fallbackLng: 'en'
   });
 
 export default i18n;
